1b10652ad35c33c4e2fb9dda1043b2edac43fd22
COOL- <87934102+msajo@users.noreply.github.com>
2023-02-08 15:05:57 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preLoad(){
+
+}
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save('Mustshe_filter_img.png');
+}
