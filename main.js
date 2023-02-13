83f04ec654972dc5fe4f7d18570904b479b55262
COOL- <87934102+msajo@users.noreply.github.com>
2023-02-13 16:07:30 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,21 @@ function preLoad(){
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPose);
+}
+function modelLoaded(){
+    console.log("poseNet has been initialized");
+
+}
+function gotPose(results){
+    if(results.length>0){
+        console.log(results);
+        
+    }
 }
 function draw(){
 
