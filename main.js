fe497ca787b5410f10a51b632d5f768f075b903d
COOL- <87934102+msajo@users.noreply.github.com>
2023-02-16 16:39:09 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+nose_x = 0;
+nose_y = 0;
 function preLoad(){
-
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup(){
     canvas = createCanvas(300,300);
@@ -17,11 +19,18 @@ function modelLoaded(){
 function gotPose(results){
     if(results.length>0){
         console.log(results);
-        
+        nose_x = results[0].pose.nose.x;
+        nose_y = results[0].pose.nose.y;
+        console.log("nose x position is " + nose_x);
+        console.log("nose y position is " + nose_y);
     }
 }
 function draw(){
-
+    image(video,0,0,300,300);
+    image(mustache,nose_x,nose_y,30,20);
+    
+    
+    
 }
 function take_snapshot(){
     save('Mustshe_filter_img.png');
